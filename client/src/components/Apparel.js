bb0d540c1930d542f39c9de7ba9cc2b55de1e22c
jimmyjr77 <jimmyjr.obrien@gmail.com>
2023-10-28 12:40:59 -0400

    trying to access user data on graphQL by modifying auth.js, resolvers.js, and typeDefs.js. Added some admin scripts and isAdmin fields. Added isAdmin to the seed information as well. Added and commented out Date scalar.

--- client/src/components/Apparel.js
+++ client/src/components/Apparel.js
@@ -1,18 +1,21 @@
 import React from 'react';
-import products from '../db/products.js'; 
 import ProductContainer from './ProductContainer.js';
 import { ApparelTitle, ApparelContainer } from '../styles/ApparelStyles';
+import { useQuery } from '@apollo/client';
+import { QUERY_PRODUCTS } from '../utils/queries';
 
 function Apparel({ addToCart }) {
+  const { loading, data } = useQuery(QUERY_PRODUCTS);
+  const productData = data?.getProducts || [];
+
   return (
     <ApparelContainer>
       <ApparelTitle>Dangerous Boys Collection</ApparelTitle>
-      {products.map((product) => (
-        <ProductContainer key={product.id} product={product} addToCart={addToCart} />
+      {productData.map((product) => (
+        <ProductContainer key={product._id} product={product} addToCart={addToCart} />
       ))}
     </ApparelContainer>
   );
 }
-
 export default Apparel;
 
--- server/models/product.js
+++ server/models/product.js
@@ -13,7 +13,7 @@ const productSchema = new mongoose.Schema({
     type: Number,
     required: true,
   },
-  imageUrls: [
+  imageUrl: [
     {
       type: String,
       required: true,
